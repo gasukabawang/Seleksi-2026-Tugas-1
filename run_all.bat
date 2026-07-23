a417da7832735836b97d94ec62306e4f48c4d790
gasukabawang <sherry.eunike@gmail.com>
2026-07-23 09:41:18 +0700

    fix: generate log file in automated scheduling and update README

--- run_all.bat
+++ run_all.bat
@@ -2,21 +2,24 @@
 echo ===== Mulai proses ETL =====
 echo Waktu mulai: %date% %time%
 
+echo ===== Mulai proses ETL ===== >> "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\log.txt"
+echo Waktu mulai: %date% %time% >> "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\log.txt"
+
 cd /d "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\Data Scraping\src"
-echo.
-echo ===== Menjalankan scraper.py =====
-python scraper.py
+echo Menjalankan scraper.py...
+python scraper.py >> "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\log.txt" 2>&1
+echo scraper.py selesai
 
 cd /d "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\Data Storing\src"
-echo.
-echo ===== Menjalankan storing.py =====
-python storing.py
+echo Menjalankan storing.py...
+python storing.py >> "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\log.txt" 2>&1
+echo storing.py selesai
 
 cd /d "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\Data Warehouse\src"
-echo.
-echo ===== Menjalankan warehouse.py =====
-python warehouse.py
+echo Menjalankan warehouse.py...
+python warehouse.py >> "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\log.txt" 2>&1
+echo warehouse.py selesai
 
-echo.
 echo ===== Proses ETL selesai =====
 echo Waktu selesai: %date% %time%
+echo Waktu selesai: %date% %time% >> "C:\Users\sherr\Projects\Seleksi-2026-Tugas-1\log.txt"
